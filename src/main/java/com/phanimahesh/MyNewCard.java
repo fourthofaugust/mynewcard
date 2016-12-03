c9232055c2c35f6265d3444a25f334e32bf49d5c
phanimahesh <phanimahesh@outlook.com>
2016-12-03 15:06:15 -0500

    Defined package

--- src/main/java/com/phanimahesh/MyNewCard.java
+++ src/main/java/com/phanimahesh/MyNewCard.java
@@ -4,4 +4,10 @@ package com.phanimahesh;
  * Created by Phani Mahesh on 12/3/2016.
  */
 public interface MyNewCard {
+
+    String generateNewCard();
+    String generateNewCard(String cardType);
+    String generateNewCard(String cardType, boolean expRequired, boolean cvvRequired);
+    String generateNewCard(String cardType, boolean expRequired, boolean cvvRequired, long quantity);
+
 }
--- src/main/java/com/phanimahesh/MyNewCardImpl.java
+++ src/main/java/com/phanimahesh/MyNewCardImpl.java
@@ -3,5 +3,21 @@ package com.phanimahesh;
 /**
  * Created by Phani Mahesh on 12/3/2016.
  */
-public class MyNewCardImpl {
+public class MyNewCardImpl implements MyNewCard{
+
+    public String generateNewCard() {
+        return null;
+    }
+
+    public String generateNewCard(String cardType) {
+        return null;
+    }
+
+    public String generateNewCard(String cardType, boolean expRequired, boolean cvvRequired) {
+        return null;
+    }
+
+    public String generateNewCard(String cardType, boolean expRequired, boolean cvvRequired, long quantity) {
+        return null;
+    }
 }
